4311323baca30c3bd28e54a01c7bb5f9381396b0
HampusEriksson <hampus.eriksson@ga.ntig.se>
2023-08-30 13:51:43 +0200

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,12 +1,15 @@
 import random
 
-comp = random.choice(["rock", "paper", "scissors"])
 
-user = input("Welcome to the game.\nPlease choose one: Rock, Paper or Scissors?").lower()
+while True:
+    comp = random.choice(["rock", "paper", "scissors"])
 
-print(f"Datorn valde {comp} och du valde {user}.")
+    user = input("Welcome to the game.\nPlease choose one: Rock, Paper or Scissors?").lower()
 
-# Din uppgift är att koda m.h.a if, elif och else ett program som skriver ut resultatet på spelet
-# rock slår scissors, scissors slår paper, paper slår rock
-# Tips: Använd and/or/nästlade if-satser för att förenkla koden
+    print(f"Datorn valde {comp} och du valde {user}.")
+
+    # Din uppgift är att koda m.h.a if, elif och else ett program som skriver ut resultatet på spelet
+    # rock slår scissors, scissors slår paper, paper slår rock
+    # Tips: Använd and/or/nästlade if-satser för att förenkla koden
+    # Extra: Lägg till att man spelar till 3 poäng istället för att spela oändligt.
 
