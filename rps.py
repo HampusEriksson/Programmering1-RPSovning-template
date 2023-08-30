6f7317affe309bbcf87b1eadbcd528e3702ef54a
HampusEriksson <hampus.eriksson@ga.ntig.se>
2023-08-30 13:45:58 +0200

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,12 @@
+import random
+
+comp = random.choice(["rock", "paper", "scissors"])
+
+user = input("Welcome to the game.\nPlease choose one: Rock, Paper or Scissors?").lower()
+
+print(f"Datorn valde {comp} och du valde {user}.")
+
+# Din uppgift är att koda m.h.a if, elif och else ett program som skriver ut resultatet på spelet
+# rock slår scissors, scissors slår paper, paper slår rock
+# Tips: Använd and/or/nästlade if-satser för att förenkla koden
+
